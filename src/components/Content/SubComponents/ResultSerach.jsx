17eb97a2db61e36b633a5fd2fa769253bba35c9a
Leandro01-oliver <leandro.oliver_lima@hotmail.com>
2021-11-13 20:32:08 -0400

    t

--- src/components/Content/SubComponents/ResultSerach.jsx
+++ src/components/Content/SubComponents/ResultSerach.jsx
@@ -5,20 +5,7 @@ function ResultSearch() {
     <>
       <section className="box-result">
         <div className="row align-items-center">
-          <div className="col-sm-6 my-3">
-            <div className="box-text-result">
-              <div className="text-result">
-                <i class="fas fa-check mx-3 "></i>
-                <span className="true-ckeck">Verdadeiro</span>
-              </div>
-              <div className="text-result">
-                {" "}
-                <i class="fas fa-times mx-3 "></i>
-                <span className="false-ckeck">Falso</span>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-6 my-3">
+          <div className="col-sm-12 my-3">
             <div className="box-porcentagem-result">
               <div className="circlo-porcem">
                 <span className="text-porcentagem">30%</span>
